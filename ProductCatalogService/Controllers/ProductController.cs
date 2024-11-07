c9168f2cae88743bf2a1a0b738c2ac1a73920b97
Alec Christensen <alec_christensen@hotmail.com>
2024-11-07 12:09:25 +0100

    "Implementerade ProductController med en grundläggande GET-metod i ProductCatalogService.API."

--- ProductCatalogService/Controllers/ProductController.cs
+++ ProductCatalogService/Controllers/ProductController.cs
@@ -1,6 +1,25 @@
-﻿namespace ProductCatalogService.API.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductCatalogService.Business.Interfaces;
+using ProductCatalogService.Data.Models;
+
+namespace ProductCatalogService.API.Controllers
 {
-    public class ProductController
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductController : ControllerBase
     {
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var products = _productService.GetProducts();
+            return Ok(products);
+        }
     }
 }
