7ec381ab572b06df634e77f29957dd51b8ceba6b
Alec Christensen <alec_christensen@hotmail.com>
2024-11-07 12:07:13 +0100

    "Implementerade ProductService och ProductRepository med grundläggande logik i ProductCatalogService."

--- ProductCatalogService.Business/Services/ProductService.cs
+++ ProductCatalogService.Business/Services/ProductService.cs
@@ -1,5 +1,22 @@
-﻿namespace ProductCatalogService.Business.Services;
+﻿using ProductCatalogService.Business.Interfaces;
+using ProductCatalogService.Data.Interfaces;
+using ProductCatalogService.Data.Models;
 
-internal class ProductService
+namespace ProductCatalogService.Business.Services;
+
+public class ProductService : IProductService
 {
+    private readonly IProductRepository _productRepository;
+
+    public ProductService(IProductRepository productRepository)
+    {
+        _productRepository = productRepository;
+    }
+
+    public IEnumerable<Product> GetProducts()
+    {
+        // Använder repository för att hämta produkter
+        return _productRepository.GetAllProducts();
+    }
 }
+
--- ProductCatalogService.Data/Repositories/ProductRepository.cs
+++ ProductCatalogService.Data/Repositories/ProductRepository.cs
@@ -1,5 +1,17 @@
-﻿namespace ProductCatalogService.Data.Repositories;
+﻿using ProductCatalogService.Data.Interfaces;
+using ProductCatalogService.Data.Models;
 
-internal class ProductRepository
+namespace ProductCatalogService.Data.Repositories;
+
+public class ProductRepository : IProductRepository
 {
+    public IEnumerable<Product> GetAllProducts()
+    {
+        // Returnerar en hårdkodad lista med produkter för enkelhetens skull
+        return new List<Product>
+        {
+            new Product { Id = 1, Name = "Product A" },
+            new Product { Id = 2, Name = "Product B" }
+        };
+    }
 }
